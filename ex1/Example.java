7b39ca0d1e4ad51ebf2539b4c06bec20c9a1bf08
Kalu Kalu <tmipanthers@gmail.com>
2017-03-13 20:45:56 +0800

    changed should to should not

--- ex1/Example.java
+++ ex1/Example.java
@@ -4,7 +4,7 @@ public class Example {
 		// Your amazing code goes here...
 		System.out.println("We are making a new PEZ Dispenzer");
 		PezDispenser dispenser = new PezDispenser();
-		dispenser.characterName = "Manu Ginobili"; //We should allow users to change this in the future
+		dispenser.characterName = "Manu Ginobili"; //We should not allow users to change this in the future
 		System.out.printf("The dispenser is %s %n",
 						   dispenser.characterName);
 	}
